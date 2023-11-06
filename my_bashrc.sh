6e8a070f82ddfa638a25a7cfeda10ed71b78cde7
Jon Cruxible <tagrous87@gmail.com>
2023-11-06 05:27:33 -0500

    my_bashrc.sh

--- my_bashrc.sh
+++ my_bashrc.sh
@@ -1,30 +1,24 @@
-#This file is for debian, tested and runs on Sparky Linux
+#This file is for debian, tested and runs on Sparky Linux and Parrot
 
-#find the ps line matching this and copy and replace
-if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]  _\w\[\033[01;31m\]\n (__\[\033[01;35m\]\u@\h\[\033[00m\]:\$ '
-else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$'
-fi
-
-#functions can be replaced to fit your needs
-
+# my additions
+#directory color pruple
+#LS_COLORS=$LS_COLORS:'di=0;35:' ; export LS_COLORS
+LS_COLORS="di=1;31:ex=1;37"
 function start_shell_or_venv() {
     if [ -d ~/bin ]
     then
       PATH=$PATH:~/bin
     fi
-    echo "What would you like to do?"
-    select option in "Start Shell" "Activate Virtual Environment" "Youtube Downloader" "Pyra Compiler" "exit"; do
+    echo -e "\033[37mWhat would you like to do?\033[0m"
+    select option in "Start Shell" "Activate Virtual Environment" "Pyra Compiler"; do
         case $option in
-            "Start Shell" ) neofetch && date && break;;
-            "Activate Virtual Environment" ) neofetch && date && cd ~/pyra_env && source bin/activate; break;;
-            "Youtube Downloader" ) cd ~/bin && download; break;;
+            "Start Shell" ) neofetch && echo -e " \033[31m$(date)\033[0m" && break;;
+            "Activate Virtual Environment" ) neofetch && echo -e " \033[31m$(date)\033[0m" && cd ~/pyra_env && source bin/activate; break;;
             "Pyra Compiler" ) compiler; break;;
-            "Exit" ) break;;
         esac
     done
 }
 
 start_shell_or_venv
-LS_COLORS=$LS_COLORS:'di=0;35:' ; export LS_COLORS
+echo -e "\033[37m Your current executables \033[0m"
+ls ~/bin
