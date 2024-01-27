7a906a4f814c603323cf67619ce35e4cbdf5a967
Jon Cruxible <tagrous87@gmail.com>
2024-01-27 03:30:43 -0500

    my_bashrc.sh

--- my_bashrc.sh
+++ my_bashrc.sh
@@ -1,48 +1,65 @@
-#This file is for debian, tested and runs on Sparky Linux and Parrot
-
 # my additions
-#directory color pruple
 #LS_COLORS=$LS_COLORS:'di=0;35:' ; export LS_COLORS
 LS_COLORS="di=1;31:ex=1;37"
+
+if [ -d ~/pyra_bin ]; then
+    PATH=$PATH:~/pyra_bin:~/pyra_bin/cli_compiler:~/pyra_bin/cli_editor:~/pyra_bin/gui_compiler:~/pyra_bin/gui_editor:~/pyra_bin/image_editor:~/pyra_bin/pyra_roku_cli:~/pyra_bin/pyra_roku_gui
+fi
+
 function start_shell_or_venv() {
-    if [ -d ~/bin ]; then
-        PATH=$PATH:~/bin
-    fi
+    echo -e "\033[01;31m ██▓███ ▓██   ██▓ ██▀███   ▄▄▄      "
+    echo -e "▓██░  ██▒▒██  ██▒▓██ ▒ ██▒▒████▄    "
+    echo -e "▓██░ ██▓▒ ▒██ ██░▓██ ░▄█ ▒▒██  ▀█▄  "
+    echo -e "▒██▄█▓▒ ▒ ░ ▐██▓░▒██▀▀█▄  ░██▄▄▄▄██ "
+    echo -e "▒██▒ ░  ░ ░ ██▒▓░░██▓ ▒██▒ ▓█   ▓██▒"
+    echo -e "▒▓▒░ ░  ░  ██▒▒▒ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░"
+    echo -e "░▒ ░     ▓██ ░▒░   ░▒ ░ ▒░  ▒   ▒▒ ░"
+    echo -e "░░       ▒ ▒ ░░    ░░   ░   ░   ▒   "
+    echo -e "         ░ ░        ░           ░  ░"
+    echo -e "         ░ ░                        "
 
     while true; do
-        echo -e "\033[37mWhat would you like to do?\033[0m"
-        select option in "Start Shell" "Activate Virtual Environment" "Pyra Compiler" "pyserver"; do
-            case $option in
-                "Start Shell" )
-                    neofetch
-                    echo -e " \033[31m$(date)\033[0m"
-                    break
-                    ;;
-                "Activate Virtual Environment" )
-                    neofetch
-                    echo -e " \033[31m$(date)\033[0m"
-                    cd ~/pyra_env && source bin/activate
-                    break
-                    ;;
-                "Pyra Compiler" )
-                    compiler
-                    break
-                    ;;
-                "pyserver" )
-                    python3 -m http.server -b 0.0.0.0
-                    break
-                    ;;
+        echo -e "C░hoose a░f░unction: \n"
+        select option in \
+        "Start Shell" \
+        "Activate Virtual Environment" \
+        "pyserver" \
+        "visualboy"
+        do
+        case $option in
+            "Start Shell" )
+                neofetch
+                echo -e " \033[31m$(date)\033[0m"
+                echo -e " \033[31mexecutables:\n pyra_compiler\n pyra_editor\n pyra_gui_compiler\n pyra_gui_editor\n pyra_roku\n roku_gui\033[0m"
+                break
+                ;;
+            "Activate Virtual Environment" )
+                neofetch
+                echo -e " \033[31m$(date)\033[0m"
+                cd ~/pyra_env && source bin/activate
+                echo -e " \033[31mexecutables:\n pyra_compiler\n pyra_editor\pyra_gui_compiler\n pyra_gui_editor\n pyra_roku\n roku_gui\033[0m"
+                break
+                ;;
+            "pyserver" )
+                python3 -m http.server -b 0.0.0.0
+                break
+                ;;
+            "visualboy" )
+                /snap/bin/visualboyadvance-m
+                break
+                ;;
             esac
         done
 
         # Check if the user entered the desired command
-        if [ "$option" == "Start Shell" ] || [ "$option" == "Activate Virtual Environment" ] || [ "$option" == "Pyra Compiler" ]; then
+        if [ "$option" == "Start Shell" ] || 
+           [ "$option" == "Activate Virtual Environment" ] || 
+           [ "$option" == "pyserver" ] || 
+           [ "$option" == "visualboy" ]
+        then
             break
         fi
     done
-}
+    }
 
 start_shell_or_venv
-
-echo -e "\033[37m Your current executables \033[0m"
-ls ~/bin
